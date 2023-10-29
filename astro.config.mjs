71b4f282c327e8ef0eb165768377d5d3c6156b70
Jack Holmes <jackholmes@onmail.com>
2023-10-29 19:48:05 +0000

    feat: :sparkles: add PurgeCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config'
 
+import purgecss from 'astro-purgecss'
+
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+    integrations: [
+        purgecss({
+            fontFace: true,
+            keyframes: true,
+            safelist: ['random', 'yep', 'button', /^nav-/],
+            blocklist: ['usedClass', /^nav-/],
+            content: [
+                process.cwd() + '/src/**/*.{astro,vue}' // Watching astro and vue sources (for SSR, read the note below)
+            ]
+        })
+    ]
+})
