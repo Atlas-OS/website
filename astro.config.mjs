d86dc8fd5f6c8990a270ed75785aba13d8ec5f8f
jacksyrm <74987167+jacksyrm@users.noreply.github.com>
2024-04-01 02:26:42 +0200

    chore: :art: lots of minor fixes and updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,15 @@
 import { defineConfig } from 'astro/config'
-import purgecss from 'astro-purgecss'
 import icon from 'astro-icon'
 import partytown from '@astrojs/partytown'
-
 import compress from 'astro-compress'
 
 // https://astro.build/config
-export default defineConfig({
+export default {
     integrations: [
-        purgecss({
-            fontFace: true,
-            keyframes: true,
-            safelist: ['random', 'yep', 'button', /^nav-/],
-            blocklist: ['usedClass', /^nav-/],
-            content: [
-                process.cwd() + '/src/**/*.{astro,vue}' // Watching astro and vue sources (for SSR, read the note below)
-            ]
-        }),
         icon({
             iconDir: 'src/assets/icons'
         }),
         partytown(),
         compress()
     ]
-})
+}
