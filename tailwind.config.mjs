a3a0a150590dc6e4b6143008183345f1cdb068fa
he3als <65787561+he3als@users.noreply.github.com>
2024-08-27 13:35:51 +0100

    Squashed commit of the following:
    commit e28d4528c531457bc210e98c452afc49e82df803
    Author: he3als <65787561+he3als@users.noreply.github.com>
    Date:   Tue Aug 27 13:03:45 2024 +0100
    
        feat: update lockfile
    
    commit 09e22092665107cbc80d16698c8ff4f002c21c10
    Author: he3als <65787561+he3als@users.noreply.github.com>
    Date:   Tue Aug 27 12:59:14 2024 +0100
    
        feat: add tailwind & popup for docs

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		screens: {
+			'sm': '500px'
+		},
+		extend: {
+			colors: {
+				"modal": "var(--bg-modal-color)"
+			}
+		}
+	},
+	plugins: [],
+}
