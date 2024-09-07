e538e31ff4cfd5f6f7b059b3a19df4d5abb8d0ba
he3als <65787561+he3als@users.noreply.github.com>
2024-09-07 23:14:11 +0100

    feat(modal): many improvements & fixes (#6)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,6 +8,16 @@ export default {
 		extend: {
 			colors: {
 				"modal": "var(--bg-modal-color)"
+			},
+			keyframes: {
+				modalOpen: {
+					'0%': { transform: 'scale(0.5)' },
+					'50%': { transform: 'scale(1.05)' },
+					'100%': { transform: 'scale(1)' },
+				}
+			},
+			animation: {
+				modalOpen: 'modalOpen .4s cubic-bezier(0.33, 1, 0.68, 1) 0s 1 normal none running',
 			}
 		}
 	},
