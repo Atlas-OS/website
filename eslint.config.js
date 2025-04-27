6d5a15868b59372fb4c239c0de48062031a5a986
Jack <74987167+jacksyrm@users.noreply.github.com>
2025-04-27 13:36:48 +0100

    chore: no more cjs! and update dependencies

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import astro from "eslint-plugin-astro";
+import astroParser from "astro-eslint-parser";
+import prettierConfig from "eslint-config-prettier";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...astro.configs["flat/recommended"],
+
+  {
+    files: ["**/*.astro"],
+    languageOptions: {
+      globals: {
+        Astro: "readonly",
+        Fragment: "readonly",
+      },
+      parser: astroParser,
+      parserOptions: {
+        parser: tseslint.parser,
+        extraFileExtensions: [".astro"],
+        project: "./tsconfig.json",
+      },
+    },
+    processor: astro.processors.astro,
+  },
+
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  },
+
+  prettierConfig,
+  {
+    ignores: ["dist/", ".astro/", "node_modules/"],
+  }
+);
